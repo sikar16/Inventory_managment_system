88b085bf674f516869c41f91934931e3ea366de1
zeru0429 <mihiretut21@gmail.com>
2024-08-30 09:48:15 -0400

    category feature type error fixed

--- codebase/frontend/src/features/admin/category/AddCategory.tsx
+++ codebase/frontend/src/features/admin/category/AddCategory.tsx
@@ -1,65 +1,69 @@
-import React, { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import InputLabel from '@mui/material/InputLabel';
-import Button from '@mui/material/Button';
-import { useAddNewProductCategoryMutation } from '../../../services/productCategorySerivce';
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
+import Button from "@mui/material/Button";
+import { useAddNewProductCategoryMutation } from "../../../services/productCategorySerivce";
+
 interface AddCategoryProps {
-    handleCloseDialog: any
+  handleCloseDialog: () => void;
 }
 
 const AddCategory: React.FC<AddCategoryProps> = ({ handleCloseDialog }) => {
-    const [customCategory, setCustomCategory] = useState('');
-    const [addCategory, { isError, isSuccess, isLoading, error }] = useAddNewProductCategoryMutation();
-    // console.log(response)
-    const handleCustomCategoryChange = (
-        event: React.MouseEvent<HTMLButtonElement>,
-    ) => {
-        setCustomCategory(event.target.value);
-    };
+  const [customCategory, setCustomCategory] = useState("");
+  const [addCategory, { isError, isSuccess, isLoading, error }] =
+    useAddNewProductCategoryMutation();
 
+  const handleCustomCategoryChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setCustomCategory(event.target.value);
+  };
 
-    const handleAddCategory = async () => {
-        const formdata = {
-            "name": customCategory
-        }
-        console.log(formdata);
-        await addCategory(formdata)
+  const handleAddCategory = async () => {
+    const formdata = {
+      name: customCategory,
     };
-    if (isSuccess) handleCloseDialog()
-    const handleDiscard = () => {
-        handleCloseDialog();
-    };
-    return (
-        <div className='mx-10 mb-10  w-[400px]'>
-            <form className='space-y-2'>
-                <InputLabel id="category-label">Category</InputLabel>
-                {isError && <p className='text-red-500 '>{error.toString()}</p>}
-                <TextField
-                    label="New Category"
-                    variant="outlined"
-                    size="small"
-                    className="w-full mt-2"
-                    value={customCategory}
-                    onChange={handleCustomCategoryChange}
-                />
-                <div className='pt-10 '>
-                    <div className='flex justify-between gap-5'>
-                        <Button variant="outlined" color="error" onClick={handleDiscard}>
-                            Discard
-                        </Button>
-                        <button
-                            type="button"
-                            disabled={isLoading}
-                            className='bg-[#002a47] py-1 px-3 text-white rounded-md'
-                            onClick={handleAddCategory}
-                        >
-                            Add Category
-                        </button>
-                    </div>
-                </div>
-            </form>
+    console.log(formdata);
+    await addCategory(formdata);
+  };
+
+  if (isSuccess) handleCloseDialog();
+
+  const handleDiscard = () => {
+    handleCloseDialog();
+  };
+
+  return (
+    <div className="mx-10 mb-10 w-[400px]">
+      <form className="space-y-2">
+        <InputLabel id="category-label">Category</InputLabel>
+        {isError && <p className="text-red-500">{error.toString()}</p>}
+        <TextField
+          label="New Category"
+          variant="outlined"
+          size="small"
+          className="w-full mt-2"
+          value={customCategory}
+          onChange={handleCustomCategoryChange} // Use the updated handler
+        />
+        <div className="pt-10">
+          <div className="flex justify-between gap-5">
+            <Button variant="outlined" color="error" onClick={handleDiscard}>
+              Discard
+            </Button>
+            <button
+              type="button"
+              disabled={isLoading}
+              className="bg-[#002a47] py-1 px-3 text-white rounded-md"
+              onClick={handleAddCategory}
+            >
+              Add Category
+            </button>
+          </div>
         </div>
-    );
-}
+      </form>
+    </div>
+  );
+};
 
-export default AddCategory
+export default AddCategory;
--- codebase/frontend/src/features/admin/category/CategoryList.tsx
+++ codebase/frontend/src/features/admin/category/CategoryList.tsx
@@ -1,84 +1,107 @@
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import AddCategory from './AddCategory';
-import { useState } from 'react';
-import CategoryTable from './CategoryTable';
-import Title from '../../../component/TablesTitle';
-import { useGetAllproductCategoryQuery } from '../../../services/productCategorySerivce';
-import Loading from '../../../component/Loading';
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import AddCategory from "./AddCategory";
+import { useState } from "react";
+import CategoryTable from "./CategoryTable";
+import Title from "../../../component/TablesTitle";
+import { useGetAllproductCategoryQuery } from "../../../services/productCategorySerivce";
+import Loading from "../../../component/Loading";
 
 export default function CategoryList() {
-    const [openDialog, setOpenDialog] = useState(false);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const handleOpenDialog = () => setOpenDialog(true);
+  const handleCloseDialog = () => setOpenDialog(false);
 
-    const handleOpenDialog = () => setOpenDialog(true);
-    const handleCloseDialog = () => setOpenDialog(false);
+  const { isError, isLoading, isSuccess, data, error } =
+    useGetAllproductCategoryQuery("productCategory");
+  const {
+    isError: isCategoryError,
+    isLoading: isCategoryLoading,
+    isSuccess: isCategorySuccess,
+    data: categories,
+    error: categoryError,
+  } = useGetAllproductCategoryQuery("get all product");
+  // console.log(data)
+  if (isError) {
+    return (
+      <div>
+        <h1>Error: {"An error occurred"}</h1>
+      </div>
+    );
+  }
 
-    const { isError, isLoading, isSuccess, data, error } = useGetAllproductCategoryQuery('productCategory');
-    const {
-        isError: isCategoryError,
-        isLoading: isCategoryLoading,
-        isSuccess: isCategorySuccess,
-        data: categories,
-        error: categoryError,
-    } = useGetAllproductCategoryQuery();
-    // console.log(data)
-    if (isError) {
-        return (
-            <div>
-                <h1>Error: {error?.message || 'An error occurred'}</h1>
-            </div>
-        );
-    }
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (isSuccess) {
+    return (
+      <div className="mt-10">
+        <Title
+          tableName="Category"
+          onClick={handleOpenDialog}
+          action="Add Category"
+        />
 
-    if (isLoading) {
-        return <Loading />;
-    }
-    if (isSuccess) {
-        return (
-            <div className='mt-10'>
-                <Title tableName="Category" action="Add Category" onClick={handleOpenDialog} />
+        <div className="flex flex-wrap gap-2 mt-10 mx-10 mb-5">
+          <div className="bg-white px-3 py-3 rounded-md mb-2 flex items-center">
+            <p className="me-3 text-gray-500">Category :</p>
+            <select className="bg-[#F5F5F5] text-gray-700">
+              {categories &&
+                categories.map((category) => (
+                  <option key={category.id} value={category.id}>
+                    {category.name}
+                  </option>
+                ))}
+            </select>
+          </div>
+        </div>
 
-                <div className='flex flex-wrap gap-2 mt-10 mx-10 mb-5'>
-                    <div className='bg-white px-3 py-3 rounded-md mb-2 flex items-center'>
-                        <p className='me-3 text-gray-500'>Category :</p>
-                        <select className='bg-[#F5F5F5] text-gray-700'>
-                            {categories.map((category) => (
-                                <option key={category.id} value={category.id}>
-                                    {category.name}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-                </div>
+        <hr className="w-full text-black bg-black" />
 
-                <hr className='w-full text-black bg-black' />
+        <div className="my-4">
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-[50%] bg-white rounded-2xl py-[5px] px-3"
+          />
+        </div>
+        <CategoryTable
+          productCategorylist={data}
+          anchorEl={anchorEl}
+          setAnchorEl={setAnchorEl}
+        />
 
-                <div className='my-4'>
-                    <input type="text" placeholder='Search' className='w-[50%] bg-white rounded-2xl py-[5px] px-3' />
-                </div>
+        <Dialog open={openDialog} onClose={handleCloseDialog}>
+          <DialogTitle>Add new category</DialogTitle>
+          <DialogContent>
+            <AddCategory handleCloseDialog={handleCloseDialog} />
+          </DialogContent>
+          <DialogActions>
+            <button onClick={handleCloseDialog} className="p-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={25}
+                height={25}
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="none"
+                  stroke="black"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={1.5}
+                  d="M6.758 17.243L12.001 12m5.243-5.243L12 12m0 0L6.758 6.757M12.001 12l5.243 5.243"
+                ></path>
+              </svg>
+            </button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
 
-                <CategoryTable productCategorylist={data} />
-
-                <Dialog open={openDialog} onClose={handleCloseDialog}>
-                    <DialogTitle>
-                        Add new category
-                    </DialogTitle>
-                    <DialogContent>
-                        <AddCategory handleCloseDialog={handleCloseDialog} />
-                    </DialogContent>
-                    <DialogActions>
-                        <button onClick={handleCloseDialog} className='p-2'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width={25} height={25} viewBox="0 0 24 24">
-                                <path fill="none" stroke="black" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M6.758 17.243L12.001 12m5.243-5.243L12 12m0 0L6.758 6.757M12.001 12l5.243 5.243"></path>
-                            </svg>
-                        </button>
-                    </DialogActions>
-                </Dialog>
-            </div>
-        );
-    }
-
-    return null;
+  return null;
 }
