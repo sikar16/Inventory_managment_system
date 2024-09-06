3580e7134fa6d44a4a0a124702e9b0417f87f85b
zeru0429 <mihiretut21@gmail.com>
2024-09-06 16:09:58 -0400

    store api complated

--- codebase/backend/src/api/store/storeRouter.js
+++ codebase/backend/src/api/store/storeRouter.js
@@ -1,12 +1,12 @@
-import express from 'express'
-import storeController from './storeController.js'
+import express from "express";
+import storeController from "./storeController.js";
+import { isAdmin } from "../../middleware/auth.js";
+const storeRouter = express.Router();
 
-const storeRouter=express.Router()
-
-storeRouter.get("/:id",storeController.getSingleStore)
-storeRouter.get("/",storeController.getAllStores)
-storeRouter.post("/",storeController.createStore)
-storeRouter.put("/:id",storeController.updateStore)
-storeRouter.delete("/:id",storeController.deleteStore)
+storeRouter.get("/:id", storeController.getSingleStore);
+storeRouter.get("/", storeController.getAllStores);
+storeRouter.post("/", [isAdmin], storeController.createStore);
+storeRouter.put("/:id", [isAdmin], storeController.updateStore);
+storeRouter.delete("/:id", [isAdmin], storeController.deleteStore);
 
 export default storeRouter;
